75b5d591eb0d96ea57c8d8c30f0c0bc17f71b5b1
Anass Erroummati <71090363+AnasErroummati@users.noreply.github.com>
2025-06-04 15:38:34 +0100

    FIRST CODE

---
+++ brokencode.py
@@ -0,0 +1,7 @@
+# Exercise:
+# Ask the user to enter a number.
+# - If the number is positive, print "Positive".
+# - If the number is negative, print "Negative".
+# - If the user enters 0, the program should stop immediately.
+# Use a loop to continuously prompt for input until 0 is entered.
+while True:
