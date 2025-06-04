38b714f8b9e4b11c372869bf3427af596bc5096a
elghandoursoufiane <mol364047@outlook.fr>
2025-06-04 19:02:23 +0200

    soufiane

--- brokencode.py
+++ brokencode.py
@@ -5,3 +5,12 @@
 # - If the user enters 0, the program should stop immediately.
 # Use a loop to continuously prompt for input until 0 is entered.
 while True:
+    nn = int(input('entreu un nombre'))
+    if nn == 0:
+        print('ffg')
+        break
+    elif nn > 0:
+        print('posi')
+    else:
+        print('negatives')
+        print('nn')
